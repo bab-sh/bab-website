07aa5f404d62fbf2198cc32e8652acda076b6e6c
Sebastian Stepper <Sebastian-Stepper@gmx.de>
2026-01-30 19:34:46 +0100

    refactor: add explicit types for commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'docs',
+        'test',
+        'build',
+        'ci',
+        'style',
+        'revert',
+        'chore',
+      ],
+    ],
+  },
 }
